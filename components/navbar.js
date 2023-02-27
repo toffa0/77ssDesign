54aa75025345115c516049aff5ca9b3941ed2e35
toffa0 <vtoff10@gmail.com>
2023-02-27 07:14:07 +0200

    first change

--- components/navbar.js
+++ components/navbar.js
@@ -73,7 +73,7 @@ const Navbar = () => {
   <nav className="navbar">
     <div className="right">
       <Link href="/">
-      <Image src="/logo1.svg" alt="Logo" className="logo" width={148} height={40} />
+      <Image src="logo1.svg" alt="Logo" className="logo" width={148} height={40} />
       </Link>
       <div className='fl fl-gap5'>
       <Link href="/categories">
