1d99d0d5aaa09b97e84f297aca780514e646c2dc
toffa0 <vtoff10@gmail.com>
2023-03-13 22:22:02 +0200

    sixth change

--- components/designerswork.js
+++ components/designerswork.js
@@ -6,7 +6,7 @@ const Designerswork = () => {
     <div className="fl designersworkcont">
                 
                     <div className="fl designersworkicon">
-                        <Image src="ppicon.svg" width={64} height={64} />
+                        <Image src="ppicon.svg" width={64} height={64} alt="" />
                     </div>
                     <div className="fl designersworkinfo">
                         
@@ -16,7 +16,7 @@ const Designerswork = () => {
                             <span className="bord2 blackkkk">Top Level</span>
                         </div>
                         <div className="fl jst1">
-                            <button className="nobg"><Image src="heart.svg" width={30} height={30} /> </button>
+                            <button className="nobg"><Image src="heart.svg" width={30} height={30} alt="" /> </button>
                             <button>Invite</button>
                         </div>
                         <div  className="fl fl-gap2">
