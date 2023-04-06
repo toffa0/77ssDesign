5c4fbe29a1ebd0047814ec44246382e96379ed9c
toffa0 <vtoff10@gmail.com>
2023-04-06 09:37:20 +0200

    23th change

--- next.config.js
+++ next.config.js
@@ -10,28 +10,3 @@ const nextConfig = {
 };
 module.exports = nextConfig
 
-// const withTM = require("next-transpile-modules")(["react-timezone-select"])
-
-// module.exports = withTM({
-//   reactStrictMode: true,
-//     images: {
-//       loader: "akamai",
-//       path: "/",
-//     },
-    
-// })
-
-
-// const withTM = require('next-transpile-modules')(['react-timezone-select']);
-
-// module.exports = withTM({
-//   reactStrictMode: true,
-//       images: {
-//         loader: "akamai",
-//         path: "/",
-//       },
-// })
-
-// module.exports = withTM({
-//   experimental: { transpilePackages: ["next-transpile-modules", "react-timezone-select"] },
-// });
