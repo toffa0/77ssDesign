c2bec087979099c83e329e56c59ce7e7c6852888
ahmed <ahmedelrai18@gmail.com>
2023-04-20 15:43:53 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
   reactStrictMode: true,
   images: {
     loader: "akamai",
     path: "/",
   },
-  
 };
-module.exports = nextConfig
-
+module.exports = nextConfig;
