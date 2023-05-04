85470617992de9564dedf10328a7217492968a93
toffa0 <vtoff10@gmail.com>
2023-05-04 17:32:09 +0300

    35th change

--- components/projectDetails/Nameinfo.js
+++ components/projectDetails/Nameinfo.js
@@ -2,12 +2,23 @@ import Image from 'next/image'
 
 
 
-const Nameinfo = ()=>{
+const Nameinfo = ({ApiData})=>{
+
 return(
     <div className='contest-cont-content fl jst-SB' id="Nameinfo">
     <div>
     <h2>Name & Info </h2>
-    <Image src="contestex1.svg" alt="" width={587} height={339} />
+    {/* <Image src="contestex1.svg" alt="" width={587} height={339} /> */}
+    <div>
+        <div>
+        <h3></h3>
+        <p>{ApiData.name}</p>
+        </div>
+        <div>
+            <h3>Description</h3>
+            <p>{ApiData.description}</p>
+        </div>
+    </div>
     </div>
     <div className='fl-col jstfe xyz'>
         <input/>
