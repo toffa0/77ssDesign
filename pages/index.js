731058ff31551b04abf17e01b84712af3af352fb
DESKTOP-TC1KCR0\somed <vtoff10@gmail.com>
2023-05-28 13:05:21 +0300

    login

--- pages/index.js
+++ pages/index.js
@@ -120,7 +120,7 @@ export default function Home() {
                     <option value="CONTEST">CONTEST </option>
                     <option value="HIRING">HIRING</option>
                   </select>
-                  <button type="submit" name="" value="Start" onClick={()=>projectType()} />
+                  <input type="submit" name="" value="Start" onClick={()=>projectType()}/>
                 </form>
 
               </div>
