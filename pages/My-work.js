ed5aa54f4754c108653bc3d136ecd1f0811cf37b
toffa0 <vtoff10@gmail.com>
2023-06-18 11:56:51 +0300

    40th change

--- pages/My-work.js
+++ pages/My-work.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Contests from "@/components/my_work/contests-mywork"
+import Contests from "@/components/my_work/contests-mywork";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import Projects from "@/components/my_work/projects-mywork";
@@ -8,71 +8,64 @@ import Ndas from "@/components/my_work/ndas-mywork";
 const Mywork = () => {
   const [activeComponent, setActiveComponent] = useState("contests");
 
-
-
   return (
     <div className="settings-container  ">
-        
+      <div className="home_section mainscr ">
+        <Navbar />
 
-        <div className="home_section mainscr ">
-        <div className="max">
-        <Navbar/>
-
-        </div>
-      <div className="max mt-133">
-        <div className="my-workkk  mb-46">
+        <div className="max mt-133">
+          <div className="my-workkk  mb-46">
             <h1 id="title">My Work</h1>
-        </div>
-      <div className="settings-nav " id="work-nav">
-        <button
-          className={` ${activeComponent === "contests" ? "active nav-btn" : "nav-btn"}`}
-          onClick={() => setActiveComponent("contests")}
-        >
-          Contests
-        </button>
+          </div>
+          <div className="settings-nav " id="work-nav">
+            <button
+              className={` ${
+                activeComponent === "contests" ? "active nav-btn" : "nav-btn"
+              }`}
+              onClick={() => setActiveComponent("contests")}
+            >
+              Contests
+            </button>
 
-        <button
-          className={`nav-btn ${
-            activeComponent === "Project" ? "active" : ""
-          }`}
-          onClick={() => setActiveComponent("Project")}
-        >
-          Project
-        </button>
-        <button
-          className={`nav-btn ${
-            activeComponent === "Designer" ? "active" : ""
-          }`}
-          onClick={() => setActiveComponent("Designer")}
-        >
-          Designer
-        </button>
-        <button
-          className={`nav-btn ${
-            activeComponent === "NDAs" ? "active" : ""
-          }`}
-          onClick={() => setActiveComponent("NDAs")}
-        >
-          NDAs
-        </button>
+            <button
+              className={`nav-btn ${
+                activeComponent === "Project" ? "active" : ""
+              }`}
+              onClick={() => setActiveComponent("Project")}
+            >
+              Project
+            </button>
+            <button
+              className={`nav-btn ${
+                activeComponent === "Designer" ? "active" : ""
+              }`}
+              onClick={() => setActiveComponent("Designer")}
+            >
+              Designer
+            </button>
+            <button
+              className={`nav-btn ${
+                activeComponent === "NDAs" ? "active" : ""
+              }`}
+              onClick={() => setActiveComponent("NDAs")}
+            >
+              NDAs
+            </button>
+          </div>
+        </div>
       </div>
-      </div>
-      </div>    
 
       <div className=" ">
-       
-      <div className="mywork-conten max">
-        {activeComponent === "contests" && <Contests />}
-        {activeComponent === "Project" && <Projects />}
-        {activeComponent === "Designer" && <Designer />}
-        {activeComponent === "NDAs" && <Ndas/>}
+        <div className="mywork-conten max">
+          {activeComponent === "contests" && <Contests />}
+          {activeComponent === "Project" && <Projects />}
+          {activeComponent === "Designer" && <Designer />}
+          {activeComponent === "NDAs" && <Ndas />}
+        </div>
       </div>
-      </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
-
-
 
 export default Mywork;
