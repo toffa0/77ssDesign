7f6cf5532440184949a230aaa768264e9e604581
toffa0 <vtoff10@gmail.com>
2023-06-20 21:22:59 +0300

    40th change

--- components/SpaceWork/NormalView.js
+++ components/SpaceWork/NormalView.js
@@ -1,19 +1,18 @@
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image'
-import { Rating } from 'react-simple-star-rating'
-import SelectedDesigners from '../selectedDesigners';
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Rating } from "react-simple-star-rating";
+import SelectedDesigners from "../selectedDesigners";
 
-
-const NormalView = ({cardData,projectID})=>{
+const NormalView = ({ cardData, projectID }) => {
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 8;
- 
+
   useEffect(() => {
     const fetchData = async () => {
       setData(cardData.cardDataProject);
-      console.log(projectID )
+      console.log(projectID);
     };
     fetchData();
   });
@@ -30,9 +29,6 @@ const NormalView = ({cardData,projectID})=>{
     });
   };
 
-
-
-
   const totalPages = Math.ceil(data.length / itemsPerPage);
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
@@ -41,50 +37,71 @@ const NormalView = ({cardData,projectID})=>{
   const handlePageClick = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
-////////////////////////////////
+  ////////////////////////////////
 
   const [SelectedCard, setSelectedCard] = useState([]);
 
   return (
-<div>
-    <div className='disc-card-cont2 jst-SB max'>
-
-      {currentData.map((item) => (
-            <div key={item.id} className='disc-card2 ' id='h-220' >
-              <span className='card-text'>#1 by osama</span>
-              <Link href={{ pathname: `/commentdesign2`, query: { id: projectID } }} style={{ marginBottom: "12px"}}>
-                
-                <Image src={item.img} id='ds-img3' alt="" width={400} height={400}/>
- 
-             
-             
-             </Link>
-             <div className='fl ali-cen jst-SB'>
-             <Rating fillColor="#00C8C8" size={30}  />
-             <button className='clearbtn'><Image src="Trash.svg" id='' alt="" width={19} height={22}/></button>
-             
-              </div>
-              <div onClick={()=>{handleItemClick(item.id)}} className='SelectedDesign' style={{cursor:"pointer"}} >
-              <Image src={SelectedCard.includes(item.id)?"GreenSelect.svg":"GreySelect.svg"}  alt="" width={26} height={26} />
-              </div>
+    <div>
+      <div className="disc-card-cont2 jst-SB ">
+        {currentData.map((item) => (
+          <div key={item.id} className="disc-card2 " id="h-220">
+            <span className="card-text">#1 by osama</span>
+            <Link
+              href={{ pathname: `/commentdesign2`, query: { id: projectID } }}
+              style={{ marginBottom: "12px" }}
+            >
+              <Image
+                src={item.img}
+                id="ds-img3"
+                alt=""
+                width={400}
+                height={400}
+              />
+            </Link>
+            <div className="fl ali-cen jst-SB">
+              <Rating fillColor="#00C8C8" size={30} />
+              <button className="clearbtn">
+                <Image src="Trash.svg" id="" alt="" width={19} height={22} />
+              </button>
             </div>
-      ))}
-
-    </div>
-    <div style={{height:"175px"}}>
-    <div className='headerpos'>
-    <SelectedDesigners className="headerpos" />
-
-    </div>
-    </div>
-    <div className="pagenumb">
+            <div
+              onClick={() => {
+                handleItemClick(item.id);
+              }}
+              className="SelectedDesign"
+              style={{ cursor: "pointer" }}
+            >
+              <Image
+                src={
+                  SelectedCard.includes(item.id)
+                    ? "GreenSelect.svg"
+                    : "GreySelect.svg"
+                }
+                alt=""
+                width={26}
+                height={26}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div style={{ height: "175px" }}>
+        <div className="headerpos">
+          <SelectedDesigners className="headerpos" />
+        </div>
+      </div>
+      <div className="pagenumb">
         {Array.from({ length: totalPages }).map((_, index) => (
-          <button key={toString(index)+toString(totalPages)+"DETAILS"} onClick={() => handlePageClick(index + 1)}>
+          <button
+            key={toString(index) + toString(totalPages) + "DETAILS"}
+            onClick={() => handlePageClick(index + 1)}
+          >
             {index + 1}
           </button>
         ))}
+      </div>
     </div>
-</div>
   );
-}
+};
 export default NormalView;
--- components/consts.js
+++ components/consts.js
@@ -1,5 +1,4 @@
-export const BASE_URL="http://ec2-54-146-11-112.compute-1.amazonaws.com"
-
+export const BASE_URL = "http://ec2-54-146-11-112.compute-1.amazonaws.com";
 
 export const listItems = [
   { id: 1, text: "Logo design" },
@@ -11,7 +10,7 @@ export const listItems = [
   { id: 7, text: "Logo & product packaging" },
   { id: 8, text: "Business card" },
   { id: 9, text: "Stationery" },
-  ];
+];
 export const listItems2 = [
   { id: 1, text: "Product packaging" },
   { id: 2, text: "Product label" },
@@ -24,8 +23,8 @@ export const listItems2 = [
   { id: 9, text: "Signage" },
   { id: 11, text: "Menu" },
   { id: 10, text: "Album cover" },
-  ];
-export const listItems3=[
+];
+export const listItems3 = [
   { id: 1, text: "Web design" },
   { id: 2, text: "WordPress theme design" },
   { id: 3, text: "Landing page design" },
@@ -37,181 +36,208 @@ export const listItems3=[
   { id: 8, text: "Animated GIFs" },
   { id: 8, text: "Logo animation" },
   { id: 8, text: "Power Point template" },
-  ];
-export const listItems4=[
+];
+export const listItems4 = [
   { id: 1, text: "T-shirt clothing / apparel" },
   { id: 2, text: "Merchandise" },
   { id: 3, text: "Cup / Mug" },
   { id: 4, text: "Sticker" },
-  ];
-export const listItems5 =[
+];
+export const listItems5 = [
   { id: 1, text: "Illustration / Graphics" },
   { id: 2, text: "Infographic" },
   { id: 3, text: "Character / Mascot" },
   { id: 4, text: "Icon / Botton" },
   { id: 5, text: "3D modeling / Animation" },
   { id: 7, text: "Store branding" },
-  ];
-  export const Logoidentity = [
-    { id: 1, text: "Logo design" },
-    { id: 2, text: "Logo & brand identity pack" },
-    { id: 3, text: "Logo & social media pack" },
-    { id: 4, text: "Logo & website Design" },
-    { id: 5, text: "Logo & business card" },
-    { id: 6, text: "Logo & Brand guide" },
-    { id: 9, text: "Logo & product packaging" },
-    { id: 7, text: "Business card" },
-    { id: 8, text: "Stationery" },
-  ];
+];
+export const Logoidentity = [
+  { id: 1, text: "Logo design" },
+  { id: 2, text: "Logo & brand identity pack" },
+  { id: 3, text: "Logo & social media pack" },
+  { id: 4, text: "Logo & website Design" },
+  { id: 5, text: "Logo & business card" },
+  { id: 6, text: "Logo & Brand guide" },
+  { id: 9, text: "Logo & product packaging" },
+  { id: 7, text: "Business card" },
+  { id: 8, text: "Stationery" },
+];
 export const Webdesign = [
-    { id: 1, text: "Web design" },
-    { id: 2, text: "WordPress theme design" },
-    { id: 3, text: "Landing page design" },
-    { id: 4, text: "App design" },
-    { id: 5, text: "Facebook cover / Post" },
-    { id: 6, text: "Social media page" },
-    { id: 7, text: "Banner ad" },
-    { id: 8, text: "Email" },
-    { id: 9, text: "Animated GIFs" },
-    { id: 10, text: "Logo animation" },
-    { id: 11, text: "Power Point template" },
-  ];
-export const ClothingMerchandise=[
-    { id: 1, text: "T-shirt clothing / apparel" },
-    { id: 2, text: "Merchandise" },
-    { id: 3, text: "Cup / Mug" },
-    { id: 4, text: "Sticker" },
-  ];
-  export const ArtIllustration =[
-    { id: 1, text: "Illustration / Graphics" },
-    { id: 2, text: "Infographic" },
-    { id: 3, text: "Character / Mascot" },
-    { id: 4, text: "Icon / Botton" },
-    { id: 5, text: "3D modeling / Animation" },
-    { id: 7, text: "Store branding" },
-  ];
-export const Businessadvertising=[
-    { id: 1, text: "Product packaging" },
-    { id: 2, text: "Product label" },
-    { id: 3, text: "Book / Magazine cover" },
-    { id: 4, text: "Flyer / Print Poster" },
-    { id: 5, text: "Card / Invitation" },
-    { id: 6, text: "Brochure" },
-    { id: 7, text: "Pag" },
-    { id: 8, text: "Car / truck / Van wrap" },
-    { id: 9, text: "Signage" },
-    { id: 11, text: "Menu" },
-    { id: 10, text: "Album cover" },
-  ];
-
-
-  export const industries =[
-    { id: 1, text: "Business & consulting" },
-    { id: 2, text: "Food & drink" },
-    { id: 3, text: "Medical & pharmaceutical" },
-    { id: 4, text: "Retail" },
-    { id: 5, text: "Technology" },
-    { id: 6, text: "Agriculture" },
-    { id: 7, text: "Animal & pet" },
-    { id: 8, text: "Architectural" },
-    { id: 9, text: "Art & design" },
-    { id: 10, text: "Attorney & law" },
-    { id: 11, text: "Automotive" },
-    { id: 12, text: "Childcare" },
-    { id: 13, text: "Cleaning & maintenance" },
-    { id: 14, text: "Communications" },
-    { id: 15, text: "Community & non-profit" },
-    { id: 16, text: "Construction" },
-    { id: 17, text: "Cosmetics & beauty" },
-    { id: 18, text: "Education" },
-    { id: 19, text: "Fashion" },
-    { id: 20, text: "Industrial" },
-    { id: 21, text: "Landscaping" },
-    { id: 22, text: "Political" },
-    { id: 23, text: "Real estate & mortgage" },
-    { id: 24, text: "Religious" },
-    { id: 25, text: "Restaurant" },
-    { id: 26, text: "Security" },
-    { id: 27, text: "Sport" },
-    { id: 28, text: "Travel & hotel" },
-    { id: 29, text: "Wedding service" },
-    
-  ];
-  export const lastActivity =[
-    { id: 1, text: "Anytime" },
-    { id: 2, text: "Online now" },
-    { id: 3, text: "Within 7 days" },
-  ];
-  export const designerLevel =[
-    
-    { id: 1, text: "Entry Level"  },
-    { id: 2, text: "Mid Level" },
-    { id: 3, text: "Advance Level" },
-  ];
-  export const Languages =[
-    { id: 1, text: "Arabic" },
-    { id: 2, text: "Chinese" },
-    { id: 3, text: "English" },
-    { id: 4, text: "French" },
-    { id: 5, text: "German" },
-    { id: 6, text: "Spanish" },
-  ];
-  export const Contacts =[
-    { id: 1, text: "Any" },
-    { id: 2, text: "Contacts only" },
-    { id: 3, text: "Hide contacts" },
-  ];
-
-  export const CatExp =[
-    { id: 1, img: "expLogo1.svg" ,width:67.56,height:36.27,text:"Logo & brand identity" },
-    { id: 2, img: "expLogo2.svg" ,width:80.93,height:57.68 ,text:"Business -Advertising "},
-    { id: 3, img: "expLogo3.svg" ,width:56.03,height:61.68 ,text:"Web - App design"},
-    { id: 4, img: "expLogo4.svg" ,width:75.52,height:60.46 ,text:"Clothing - Merchandise"},
-    { id: 5, img: "expLogo5.svg" ,width:48.46,height:46.63 ,text:"Illustration - Graphics "},
- 
-  ];
-
-  export const data =[
-    { id: 1, text: "Arabic" },
-    { id: 2, text: "Chinese" },
-    { id: 3, text: "English" },
-    { id: 4, text: "French" },
-    { id: 5, text: "German" },
-    { id: 6, text: "Spanish" },
-    { id: 1, text: "Arabic2" },
-    { id: 2, text: "Chinese2" },
-    { id: 3, text: "English2" },
-    { id: 4, text: "French" },
-    { id: 5, text: "German" },
-    { id: 6, text: "Spanish" },
-  ];
-export const cardData2=[
-  { id: 1, img:"subex1.svg"},
-  { id: 2, img:"greyrect.svg"},
-  { id: 3, img:"greyrect.svg"},        
-  { id: 4, img:"greyrect.svg"},
-  { id: 5, img:"greyrect.svg"},
-  { id: 6, img:"greyrect.svg"},
-  { id: 7, img:"greyrect.svg"},
-  { id: 8, img:"greyrect.svg"},
-  { id: 9, img:"greyrect.svg"},
-  { id: 10, img:"greyrect.svg"},
-  { id: 11, img:"greyrect.svg"},
-  { id: 12, img:"greyrect.svg"},
+  { id: 1, text: "Web design" },
+  { id: 2, text: "WordPress theme design" },
+  { id: 3, text: "Landing page design" },
+  { id: 4, text: "App design" },
+  { id: 5, text: "Facebook cover / Post" },
+  { id: 6, text: "Social media page" },
+  { id: 7, text: "Banner ad" },
+  { id: 8, text: "Email" },
+  { id: 9, text: "Animated GIFs" },
+  { id: 10, text: "Logo animation" },
+  { id: 11, text: "Power Point template" },
 ];
-export const cardDataProject=[
-  { id: 1, img:"subex1.svg"},
-  { id: 2, img:"subex1.svg"},
+export const ClothingMerchandise = [
+  { id: 1, text: "T-shirt clothing / apparel" },
+  { id: 2, text: "Merchandise" },
+  { id: 3, text: "Cup / Mug" },
+  { id: 4, text: "Sticker" },
 ];
-export const AllCategoriesItems=[
-  { id: 1, Text:"Logo-brand identity"},
-  { id: 2, Text:"Business-Advertising"},
-  { id: 3, Text:"Web-App design"},        
-  { id: 4, Text:"Clothing-Merchandise"},
-  { id: 5, Text:"Illustration-Graphics"},
+export const ArtIllustration = [
+  { id: 1, text: "Illustration / Graphics" },
+  { id: 2, text: "Infographic" },
+  { id: 3, text: "Character / Mascot" },
+  { id: 4, text: "Icon / Botton" },
+  { id: 5, text: "3D modeling / Animation" },
+  { id: 7, text: "Store branding" },
+];
+export const Businessadvertising = [
+  { id: 1, text: "Product packaging" },
+  { id: 2, text: "Product label" },
+  { id: 3, text: "Book / Magazine cover" },
+  { id: 4, text: "Flyer / Print Poster" },
+  { id: 5, text: "Card / Invitation" },
+  { id: 6, text: "Brochure" },
+  { id: 7, text: "Pag" },
+  { id: 8, text: "Car / truck / Van wrap" },
+  { id: 9, text: "Signage" },
+  { id: 11, text: "Menu" },
+  { id: 10, text: "Album cover" },
+];
 
+export const industries = [
+  { id: 1, text: "Business & consulting" },
+  { id: 2, text: "Food & drink" },
+  { id: 3, text: "Medical & pharmaceutical" },
+  { id: 4, text: "Retail" },
+  { id: 5, text: "Technology" },
+  { id: 6, text: "Agriculture" },
+  { id: 7, text: "Animal & pet" },
+  { id: 8, text: "Architectural" },
+  { id: 9, text: "Art & design" },
+  { id: 10, text: "Attorney & law" },
+  { id: 11, text: "Automotive" },
+  { id: 12, text: "Childcare" },
+  { id: 13, text: "Cleaning & maintenance" },
+  { id: 14, text: "Communications" },
+  { id: 15, text: "Community & non-profit" },
+  { id: 16, text: "Construction" },
+  { id: 17, text: "Cosmetics & beauty" },
+  { id: 18, text: "Education" },
+  { id: 19, text: "Fashion" },
+  { id: 20, text: "Industrial" },
+  { id: 21, text: "Landscaping" },
+  { id: 22, text: "Political" },
+  { id: 23, text: "Real estate & mortgage" },
+  { id: 24, text: "Religious" },
+  { id: 25, text: "Restaurant" },
+  { id: 26, text: "Security" },
+  { id: 27, text: "Sport" },
+  { id: 28, text: "Travel & hotel" },
+  { id: 29, text: "Wedding service" },
 ];
-export      const industriesMenu =[
+export const lastActivity = [
+  { id: 1, text: "Anytime" },
+  { id: 2, text: "Online now" },
+  { id: 3, text: "Within 7 days" },
+];
+export const designerLevel = [
+  { id: 1, text: "Entry Level" },
+  { id: 2, text: "Mid Level" },
+  { id: 3, text: "Advance Level" },
+];
+export const Languages = [
+  { id: 1, text: "Arabic" },
+  { id: 2, text: "Chinese" },
+  { id: 3, text: "English" },
+  { id: 4, text: "French" },
+  { id: 5, text: "German" },
+  { id: 6, text: "Spanish" },
+];
+export const Contacts = [
+  { id: 1, text: "Any" },
+  { id: 2, text: "Contacts only" },
+  { id: 3, text: "Hide contacts" },
+];
+
+export const CatExp = [
+  {
+    id: 1,
+    img: "expLogo1.svg",
+    width: 67.56,
+    height: 36.27,
+    text: "Logo & brand identity",
+  },
+  {
+    id: 2,
+    img: "expLogo2.svg",
+    width: 80.93,
+    height: 57.68,
+    text: "Business -Advertising ",
+  },
+  {
+    id: 3,
+    img: "expLogo3.svg",
+    width: 56.03,
+    height: 61.68,
+    text: "Web - App design",
+  },
+  {
+    id: 4,
+    img: "expLogo4.svg",
+    width: 75.52,
+    height: 60.46,
+    text: "Clothing - Merchandise",
+  },
+  {
+    id: 5,
+    img: "expLogo5.svg",
+    width: 48.46,
+    height: 46.63,
+    text: "Illustration - Graphics ",
+  },
+];
+
+export const data = [
+  { id: 1, text: "Arabic" },
+  { id: 2, text: "Chinese" },
+  { id: 3, text: "English" },
+  { id: 4, text: "French" },
+  { id: 5, text: "German" },
+  { id: 6, text: "Spanish" },
+  { id: 1, text: "Arabic2" },
+  { id: 2, text: "Chinese2" },
+  { id: 3, text: "English2" },
+  { id: 4, text: "French" },
+  { id: 5, text: "German" },
+  { id: 6, text: "Spanish" },
+];
+export const cardData2 = [
+  { id: 1, img: "subex1.svg" },
+  { id: 2, img: "greyrect.svg" },
+  { id: 3, img: "greyrect.svg" },
+  { id: 4, img: "greyrect.svg" },
+  { id: 5, img: "greyrect.svg" },
+  { id: 6, img: "greyrect.svg" },
+  { id: 7, img: "greyrect.svg" },
+  { id: 8, img: "greyrect.svg" },
+  { id: 9, img: "greyrect.svg" },
+  { id: 10, img: "greyrect.svg" },
+  { id: 11, img: "greyrect.svg" },
+  { id: 12, img: "greyrect.svg" },
+];
+export const cardDataProject = [
+  { id: 1, img: "subex1.svg" },
+  { id: 2, img: "subex1.svg" },
+  { id: 3, img: "subex1.svg" },
+  { id: 4, img: "subex1.svg" },
+];
+export const AllCategoriesItems = [
+  { id: 1, Text: "Logo-brand identity" },
+  { id: 2, Text: "Business-Advertising" },
+  { id: 3, Text: "Web-App design" },
+  { id: 4, Text: "Clothing-Merchandise" },
+  { id: 5, Text: "Illustration-Graphics" },
+];
+export const industriesMenu = [
   { id: 1, label: "Business & consulting" },
   { id: 2, label: "Food & drink" },
   { id: 3, label: "Medical & pharmaceutical" },
@@ -244,17 +270,15 @@ export      const industriesMenu =[
   { id: 29, label: "Others" },
 ];
 
-
-
 export const AllCategoriesAll = [
-  { id: 110, header: "Logo & identity" ,cat:'LOGO'},
-  { id: 111, text: "Logo design",cat:'LOGO' },
-  { id: 112, text: "Logo & brand identity pack",cat:'LOGO' },
-  { id: 113, text: "Logo & social media pack",cat:'LOGO' },
-  { id: 114, text: "Logo & website Design" ,cat:'LOGO'},
-  { id: 5, text: "Logo & business card" ,cat:'LOGO'},
-  { id: 6, text: "Logo & Brand guide" ,cat:'LOGO'},
-  { id: 46, text: "Logo & product packaging",cat:'LOGO'},
+  { id: 110, header: "Logo & identity", cat: "LOGO" },
+  { id: 111, text: "Logo design", cat: "LOGO" },
+  { id: 112, text: "Logo & brand identity pack", cat: "LOGO" },
+  { id: 113, text: "Logo & social media pack", cat: "LOGO" },
+  { id: 114, text: "Logo & website Design", cat: "LOGO" },
+  { id: 5, text: "Logo & business card", cat: "LOGO" },
+  { id: 6, text: "Logo & Brand guide", cat: "LOGO" },
+  { id: 46, text: "Logo & product packaging", cat: "LOGO" },
   { id: 7, text: "Business card" },
   { id: 8, text: "Stationery" },
   { id: 9, header: "Web - UI/UX design" },
@@ -295,8 +319,8 @@ export const AllCategoriesAll = [
   { id: 1145, text: "Album cover" },
 ];
 
-export const LogoidentityCat ={
-  image:'CatImage1.svg',
+export const LogoidentityCat = {
+  image: "CatImage1.svg",
   data: [
     { id: 1, text: "Logo design" },
     { id: 2, text: "Logo & brand identity pack" },
@@ -307,12 +331,12 @@ export const LogoidentityCat ={
     { id: 9, text: "Logo & product packaging" },
     { id: 7, text: "Business card" },
     { id: 8, text: "Stationery" },
-  ]
-}
- 
-export const WebdesignCat ={
-  image:"CatImage4.svg",
-  data:[
+  ],
+};
+
+export const WebdesignCat = {
+  image: "CatImage4.svg",
+  data: [
     { id: 1, text: "Web design" },
     { id: 2, text: "WordPress theme design" },
     { id: 3, text: "Landing page design" },
@@ -325,18 +349,18 @@ export const WebdesignCat ={
     { id: 10, text: "Animated GIFs" },
     { id: 11, text: "Email" },
   ],
-} 
-export const ClothingMerchandiseCat={
-  image:"CatImage5.svg",
+};
+export const ClothingMerchandiseCat = {
+  image: "CatImage5.svg",
   data: [
     { id: 1, text: "T-shirt clothing / apparel" },
     { id: 2, text: "Merchandise" },
     { id: 3, text: "Cup / Mug" },
     { id: 4, text: "Sticker" },
-  ]
-}
-export const ArtIllustrationCat ={
-  image:"CatImage3.svg",
+  ],
+};
+export const ArtIllustrationCat = {
+  image: "CatImage3.svg",
   data: [
     { id: 1, text: "Illustration / Graphics" },
     { id: 2, text: "Infographic" },
@@ -344,11 +368,11 @@ export const ArtIllustrationCat ={
     { id: 4, text: "Icon / Botton" },
     { id: 5, text: "3D modeling / Animation" },
     { id: 7, text: "Store branding" },
-  ]
-}
-export const BusinessadvertisingCat={
-  image:"CatImage2.svg",
-  data:[
+  ],
+};
+export const BusinessadvertisingCat = {
+  image: "CatImage2.svg",
+  data: [
     { id: 1, text: "Product packaging" },
     { id: 2, text: "Product label" },
     { id: 3, text: "Book / Magazine cover" },
@@ -360,12 +384,10 @@ export const BusinessadvertisingCat={
     { id: 10, text: "Menu" },
     { id: 11, text: "Album cover" },
     { id: 12, text: "Pag" },
-  ]
-} 
+  ],
+};
 
-export const Others={
-  image:"",
-  data:[
-    { id: 1, text: "Chat Bot" },
-  ]
-} 
+export const Others = {
+  image: "",
+  data: [{ id: 1, text: "Chat Bot" }],
+};
